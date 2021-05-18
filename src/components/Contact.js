788c3021115590fb6a0708ca5e02851b9732302c
Ravi Singh <ravi@appsimpact.com>
2021-05-18 23:43:35 +0530

    added contact us form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,49 @@ import React from 'react';
 const Contact = () => {
   return(
     <React.Fragment>
-      <div className="">
-        <h1>This is contact us page</h1>
+      <div className="container">
+        <div className="row mt-5 mb-5">
+          <div className="col-lg-8 col-md-12 col-12 mx-lg-auto">
+            <div className="card">
+              <div className="card-body">
+                <form>
+                  <div className="col-lg-12 mb-4">
+                    <div className="form-group">
+                      <label>Name: </label>
+                      <input type="text" className="form-control form-control-lg" />
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12 mb-4">
+                    <div className="form-group">
+                      <label>Email: </label>
+                      <input type="email" className="form-control form-control-lg" />
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12 mb-4">
+                    <div className="form-group">
+                      <label>Contact: </label>
+                      <input type="text" className="form-control form-control-lg" />
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12 mb-4">
+                    <div className="form-group">
+                      <label>Message: </label>
+                      <textarea className="form-control form-control-lg" rows="8"></textarea>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12 mb-4">
+                    <button type="submit" className="btn btn-primary btn-lg">Submit</button>
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   ) 
